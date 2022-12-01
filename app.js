c2f2f700a0ab0c7c957b020303846c2b9623fac4
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2022-12-02 06:20:19 +0900

    add slider-video

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ menuBtn.addEventListener('click', () => {
   navigation.classList.toggle('active');
   console.log(menuBtn);
 });
+
+// video slider navigation
+const btns = document.querySelectorAll('.nav-btn');
+const slides = document.querySelectorAll('.video-slide');
+
+let sliderNav = function (manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove('active');
+  });
+
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+
+  btns[manual].classList.add('active');
+  slides[manual].classList.add('active');
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    sliderNav(i);
+  });
+});
